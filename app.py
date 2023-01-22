cae69ca6812358ad72337e5dfb1c57d2405cded1
Deepak Suthar <codewithdeepak@yahoo.com>
2023-01-22 16:31:52 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import base64
+from io import BytesIO
+from rembg import remove
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def main():
+    return 'Hello World'
+
+
+@app.route('/upload', methods=['POST'])
+def uploadFile():
+    file = request.files['image'].read()
+
+    output = BytesIO(remove(file))
+    return send_file(output.getbuffer(), mimetype='image/png')
