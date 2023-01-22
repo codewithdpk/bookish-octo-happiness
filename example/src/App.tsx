7ef174c4e600e2525e34a49f1aea4994773dd086
Deepak Suthar <codewithdeepak@yahoo.com>
2023-01-22 18:19:44 +0530

    tidy-up

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,46 +1,56 @@
+import { Grid, Typography } from "@mui/material";
+import { Container, Stack } from "@mui/system";
+import axios from "axios";
 import React, { useState } from "react";
-import "./App.css";
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
+  const [outputFile, setOutputFile] = useState<File | null>(null);
 
   const changeHandler = (event: any) => {
     setFile(event.target.files[0]);
   };
 
-  const uploadFile = () => {
+  const uploadFile = async () => {
     if (file) {
-      fetch("http://127.0.0.1:5000/upload", {
-        method: "POST",
-        headers: {
-          "Content-Type": "multipart/file",
-        },
-        body: JSON.stringify({
-          image: file,
-        }),
-      })
-        .then((data) => data.json())
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((e) => {
-          console.error(JSON.stringify(e));
-        });
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const res = await axios.post("http://127.0.0.1:5000/upload", formData);
+      if (res?.status === 200) {
+        setOutputFile(res?.data);
+      }
     } else {
       alert("Please select file");
     }
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <input
-          type="file"
-          placeholder="select your file"
-          onChange={changeHandler}
-        />
-        <button onClick={uploadFile}>Upload</button>
-      </header>
-    </div>
+    <Container sx={{ pt: 6 }}>
+      <Grid container spacing={3}>
+        <Grid item sm={12} md={12} lg={12}>
+          <input
+            type="file"
+            placeholder="select your file"
+            onChange={changeHandler}
+          />
+          <button onClick={uploadFile}>Upload</button>
+        </Grid>
+        <Grid item sm={12} md={6} lg={6}>
+          <Stack direction="column">
+            <Typography variant="subtitle1">Input:</Typography>
+            {file && <img src={URL.createObjectURL(file)} alt="file" />}
+          </Stack>
+        </Grid>
+        <Grid item sm={12} md={6} lg={6}>
+          <Stack direction="column">
+            <Typography variant="subtitle1">Input:</Typography>
+            {outputFile && (
+              <img src={`data:image/jpeg;base64,${outputFile}`} alt="file" />
+            )}
+          </Stack>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
